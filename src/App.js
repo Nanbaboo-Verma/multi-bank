7087c7375522e121388a3b4a643c55ede5c9e361
Vishal Raj <mac@Macs-MacBook-Air.local>
2023-09-25 22:18:56 +0530

    Remove unused codes and some changes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Footer from "./components/footer/footer";
 import Header from "./components/header/header";
 import Hero from "./components/hero/hero";
 import Offer from "./components/offer/offer";
-import Product from "./components/product/product";
 import Map from "./components/map/map";
 import Questions from "./components/question/questions";
 import ReadFaqs from "./components/read-faqs/read-faqs";
